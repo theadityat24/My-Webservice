819d6d5bd14051aba51da1f0babdd9ce15f5a576
Aditya Tiwari <52205250+theadityat24@users.noreply.github.com>
2020-06-24 09:49:10 -0500

    switched to Flask

---
+++ api.py
@@ -0,0 +1,23 @@
+import flask
+from flask import jsonify, request
+
+from fastai.vision import load_learner\
+
+from io import BytesIO
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+tree_learner = load_learner('export_learners', 'trees.pkl')
+
+
+@app.route('/trees', methods=['GET'])
+def trees():
+    if 'img' in request.args:
+        img = open_image(BytesIO(request.args['img']))
+        prediction = int(tree_learner.predict(img))
+        return jsonify(
+            {
+                'prediction': prediction
+            }
+        )
